72a5d90143d4a6239e7a958fa879e7e5b79ae6d9
arsyl <arsylslametamrulloh@gmail.com>
2025-10-13 15:05:03 +0700

    feat: store and edit kelurahan

--- src/app/main/referensi/kelurahan/form/index.tsx
+++ src/app/main/referensi/kelurahan/form/index.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,155 +12,217 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
-import { destroyDrawerModal, stringifyError } from "../../../../../../helper"
-import { context } from "../../../../../../store"
-import { getKabKota, getProvinsi, postKelurahan, putKelurahan } from "../../../../../../services"
-import { useRouter } from "next/navigation"
-import { FC, useEffect, useState } from "react"
-import { ReloadIcon } from "@radix-ui/react-icons"
-import Combobox from "@/components/tools/combobox"
-import { FormCustomOption } from "../../../../../../store/state"
-import { KabKotaI } from "../../kabupaten-kota/columns"
-import { KelurahanI } from "../columns"
-import { ProvinsiI } from "../../provinsi/columns"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { destroyDrawerModal, stringifyError } from "../../../../../../helper";
+import { context } from "../../../../../../store";
+import {
+  getKabKota,
+  getKecamatan,
+  getProvinsi,
+  postKelurahan,
+  putKelurahan,
+} from "../../../../../../services";
+import { useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import Combobox from "@/components/tools/combobox";
+import { FormCustomOption } from "../../../../../../store/state";
+import { KabKotaI } from "../../kabupaten-kota/columns";
+import { KecamatanI } from "../../kecamatan/columns";
+import { KelurahanI } from "../columns";
+import { ProvinsiI } from "../../provinsi/columns";
 
 const FormSchema = z.object({
   // kodeProvinsi: z.string({required_error: 'Kode Provinsi tidak boleh kosong'}),
-  nama: z.string({required_error: 'Nama Kecamatan tidak boleh kosong'}).min(1, {
-    message: "Nama Kecamatan harus lebih dari 1 karakter.",
-  }),
-  id_provinsi: z.string({required_error: 'Nama Provinsi tidak boleh kosong'}).min(1, {
-    message: "Nama Provinsi harus lebih dari 1 karakter.",
-  }),
-  id_kabupaten_kota: z.string({required_error: 'Nama Kab/Kota tidak boleh kosong'}).min(1, {
-    message: "Nama Kab/Kota harus lebih dari 1 karakter.",
-  }),
-})
+  nama: z
+    .string({ required_error: "Nama Kecamatan tidak boleh kosong" })
+    .min(1, {
+      message: "Nama Kecamatan harus lebih dari 1 karakter.",
+    }),
+  id_provinsi: z
+    .string({ required_error: "Nama Provinsi tidak boleh kosong" })
+    .min(1, {
+      message: "Nama Provinsi harus lebih dari 1 karakter.",
+    }),
+  id_kabupaten_kota: z
+    .string({ required_error: "Nama Kab/Kota tidak boleh kosong" })
+    .min(1, {
+      message: "Nama Kab/Kota harus lebih dari 1 karakter.",
+    }),
+  id_kecamatan: z
+    .string({ required_error: "Nama Kecamatan tidak boleh kosong" })
+    .min(1, {
+      message: "Nama Kecamatan harus lebih dari 1 karakter.",
+    }),
+});
 
 interface FromDataI {
-  trigger: ()=>void
-  data?: KelurahanI
+  trigger: () => void;
+  data?: KelurahanI;
 }
-const FormData:FC<FromDataI> = ({ trigger,data })=> {
-  const ctx = context()
-  const router = useRouter()
+const FormData: FC<FromDataI> = ({ trigger, data }) => {
+  const ctx = context();
+  const router = useRouter();
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-  })
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [provinsiOptions, setProvinsiOptions] = useState<Array<FormCustomOption>>([])
-  const [allKabKota, setAllKabKota] = useState<Array<any>>([])
-  const [kabKotaOptions, setKabKotaOptions] = useState<Array<FormCustomOption>>([])
-  const [isKotaLoading,setIsKotaLoading] = useState<boolean>(false)
-  
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [provinsiOptions, setProvinsiOptions] = useState<
+    Array<FormCustomOption>
+  >([]);
+  const [allKabKota, setAllKabKota] = useState<Array<any>>([]);
+  const [kabKotaOptions, setKabKotaOptions] = useState<Array<FormCustomOption>>(
+    []
+  );
+  const [isKotaLoading, setIsKotaLoading] = useState<boolean>(false);
+  const [allKecamatan, setAllKecamatan] = useState<Array<any>>([]);
+  const [kecamatanOptions, setKecamatanOptions] = useState<
+    Array<FormCustomOption>
+  >([]);
+  const [isKecamatanLoading, setIsKecamatanLoading] = useState<boolean>(false);
 
-  const onSubmit = async (dataForm: z.infer<typeof FormSchema>) =>{
-    setIsLoading(true)
-    if (!data){
-      await postKelurahan(router,dataForm)
-      .then((res)=>{
-        toast({
-          title: `Berhasil menambahkan data kelurahan ${form.getValues('nama')}!`,
+  const onSubmit = async (dataForm: z.infer<typeof FormSchema>) => {
+    setIsLoading(true);
+    if (!data) {
+      await postKelurahan(router, dataForm)
+        .then((res) => {
+          toast({
+            title: `Berhasil menambahkan data kelurahan ${form.getValues(
+              "nama"
+            )}!`,
+          });
+          trigger();
+          return destroyDrawerModal(ctx);
         })
-        trigger()
-        return destroyDrawerModal(ctx)
-      })
-      .catch((err)=>{
-        console.log('err: ',err);      
-        toast({
+        .catch((err) => {
+          console.log("err: ", err);
+          toast({
             title: stringifyError(err.response.data?.message),
-            variant: 'destructive'
+            variant: "destructive",
+          });
         })
-      })
-      .finally(()=>{
-        setTimeout(()=>{
-          setIsLoading(false)
-        },500)
-      })
-    }
-    else {
-      await putKelurahan(router,dataForm,data.id)
-      .then(()=>{
-        toast({
-          title: `Berhasil mengubah data kelurahan ${form.getValues('nama')}!`,
+        .finally(() => {
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 500);
+        });
+    } else {
+      await putKelurahan(router, dataForm, data.id)
+        .then(() => {
+          toast({
+            title: `Berhasil mengubah data kelurahan ${form.getValues(
+              "nama"
+            )}!`,
+          });
+          trigger();
+          return destroyDrawerModal(ctx);
         })
-        trigger()
-        return destroyDrawerModal(ctx)
-      })
-      .catch((err)=>{
-        console.log('err: ',err);      
-        toast({
+        .catch((err) => {
+          console.log("err: ", err);
+          toast({
             title: stringifyError(err.response.data?.message),
-            variant: 'destructive'
+            variant: "destructive",
+          });
         })
-      })
-      .finally(()=>{
-        setTimeout(()=>{
-          setIsLoading(false)
-        },500)
-      })
+        .finally(() => {
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 500);
+        });
     }
-  }
+  };
 
-  const firstInit = async()=> {
-    setIsLoading(true)
+  const firstInit = async () => {
+    setIsLoading(true);
     await Promise.all([
-        getProvinsi(router,'?limit=99'),
-        getKabKota(router,'?limit=999'),
-        // getKecamatan(router,'?limit=9999')
+      getProvinsi(router, "?limit=99"),
+      getKabKota(router, "?limit=999"),
+      getKecamatan(router, "?limit=9999"),
     ])
-    .then((responses)=>{
-        const [province,kab_kota] = responses
-        
-        const provinces = province.data.data.map((item:ProvinsiI) => ({
-            value: item.id.toString(),
-            label: item.nama,
-        }));
-        setProvinsiOptions(provinces)
-        setAllKabKota(kab_kota.data.data)
-        if (data){          
-          getKotaByProvince(data.id_provinsi,kab_kota.data.data)
-          form.setValue('id_provinsi',data.id_provinsi.toString())
-          form.setValue('nama',data.nama)
-          form.setValue('id_kabupaten_kota',data.id_kabupaten_kota.toString())
-        }
-        
-    })
-    .catch((err)=>{
-        console.log('Err: ',err);
-    })
-    .finally(()=>{
-      setIsLoading(false)
-    })
-  }
-  
-  useEffect(()=>{
-      firstInit()
-      /* eslint-disable react-hooks/exhaustive-deps */
-  },[])
+      .then((responses) => {
+        const [province, kab_kota, kecamatan] = responses;
 
-  const getKotaByProvince = (id:string|number, DataAllKabKota:Array<any>=allKabKota)=> {
-    setIsKotaLoading(true)
-    try {
-        const kotaYangCocok:KabKotaI[] = DataAllKabKota.filter((item:KabKotaI) => item.id_provinsi.toString() === id.toString());
-        const kotaFiltered = kotaYangCocok.map((item:KabKotaI) => ({
-            value: item.id.toString(),
-            label: item.nama,
+        const provinces = province.data.data.map((item: ProvinsiI) => ({
+          value: item.id.toString(),
+          label: item.nama,
         }));
-        
-        setKabKotaOptions(kotaFiltered)
-        form.setValue('id_kabupaten_kota','')
+        setProvinsiOptions(provinces);
+        setAllKabKota(kab_kota.data.data);
+        setAllKecamatan(kecamatan.data.data);
+        if (data) {
+          getKotaByProvince(data.id_provinsi, kab_kota.data.data);
+          getKecamatanByKota(data.id_kabupaten_kota, kecamatan.data.data);
+          form.setValue("id_provinsi", data.id_provinsi.toString());
+          form.setValue("nama", data.nama);
+          form.setValue("id_kabupaten_kota", data.id_kabupaten_kota.toString());
+          form.setValue("id_kecamatan", data.id_kecamatan.toString());
+        }
+      })
+      .catch((err) => {
+        console.log("Err: ", err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    firstInit();
+    /* eslint-disable react-hooks/exhaustive-deps */
+  }, []);
+
+  const getKotaByProvince = (
+    id: string | number,
+    DataAllKabKota: Array<any> = allKabKota
+  ) => {
+    setIsKotaLoading(true);
+    try {
+      const kotaYangCocok: KabKotaI[] = DataAllKabKota.filter(
+        (item: KabKotaI) => item.id_provinsi.toString() === id.toString()
+      );
+      const kotaFiltered = kotaYangCocok.map((item: KabKotaI) => ({
+        value: item.id.toString(),
+        label: item.nama,
+      }));
+
+      setKabKotaOptions(kotaFiltered);
+      form.setValue("id_kabupaten_kota", "");
     } catch (error) {
-        console.log('Err: ',error);   
+      console.log("Err: ", error);
     } finally {
-        setTimeout(()=>{
-          setIsKotaLoading(false)
-        },500)
-    }   
-}
+      setTimeout(() => {
+        setIsKotaLoading(false);
+      }, 500);
+    }
+  };
+
+  const getKecamatanByKota = (
+    id: string | number,
+    DataAllKecamatan: Array<any> = allKecamatan
+  ) => {
+    setIsKecamatanLoading(true);
+    try {
+      const kecamatanYangCocok: KecamatanI[] = DataAllKecamatan.filter(
+        (item: KecamatanI) =>
+          item.id_kabupaten_kota.toString() === id.toString()
+      );
+      const kecamatanFiltered = kecamatanYangCocok.map((item: KecamatanI) => ({
+        value: item.id.toString(),
+        label: item.nama,
+      }));
+
+      setKecamatanOptions(kecamatanFiltered);
+      form.setValue("id_kecamatan", "");
+    } catch (error) {
+      console.log("Err: ", error);
+    } finally {
+      setTimeout(() => {
+        setIsKecamatanLoading(false);
+      }, 500);
+    }
+  };
 
   return (
     <Form {...form}>
@@ -171,16 +233,16 @@ const FormData:FC<FromDataI> = ({ trigger,data })=> {
           render={({ field }) => (
             <FormItem>
               <FormLabel>Nama Provinsi</FormLabel>
-              <Combobox 
-                  options={provinsiOptions}
-                  placeholder="Pilih Provinsi"
-                  value={field.value}
-                  onSelect={(e)=>{
-                    form.setValue("id_provinsi",e)
-                    getKotaByProvince(e)
-                  }}
-                  // isLoading={isRegionalLoading}
-                  disabled={provinsiOptions.length === 0}
+              <Combobox
+                options={provinsiOptions}
+                placeholder="Pilih Provinsi"
+                value={field.value}
+                onSelect={(e) => {
+                  form.setValue("id_provinsi", e);
+                  getKotaByProvince(e);
+                }}
+                // isLoading={isRegionalLoading}
+                disabled={provinsiOptions.length === 0}
               />
             </FormItem>
           )}
@@ -191,15 +253,35 @@ const FormData:FC<FromDataI> = ({ trigger,data })=> {
           render={({ field }) => (
             <FormItem>
               <FormLabel>Nama Kab/Kota</FormLabel>
-              <Combobox 
-                  options={kabKotaOptions}
-                  placeholder="Pilih Kab/Kota"
-                  value={field.value}
-                  onSelect={(e)=>{
-                    form.setValue('id_kabupaten_kota',e)
-                  }}
-                  isLoading={isKotaLoading}
-                  disabled={kabKotaOptions.length === 0 || isKotaLoading}
+              <Combobox
+                options={kabKotaOptions}
+                placeholder="Pilih Kab/Kota"
+                value={field.value}
+                onSelect={(e) => {
+                  form.setValue("id_kabupaten_kota", e);
+                  getKecamatanByKota(e);
+                }}
+                isLoading={isKotaLoading}
+                disabled={kabKotaOptions.length === 0 || isKotaLoading}
+              />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="id_kecamatan"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nama Kecamatan</FormLabel>
+              <Combobox
+                options={kecamatanOptions}
+                placeholder="Pilih Kecamatan"
+                value={field.value}
+                onSelect={(e) => {
+                  form.setValue("id_kecamatan", e);
+                }}
+                isLoading={isKecamatanLoading} // <- ganti ini
+                disabled={kecamatanOptions.length === 0 || isKecamatanLoading} // <- ganti ini
               />
             </FormItem>
           )}
@@ -209,9 +291,9 @@ const FormData:FC<FromDataI> = ({ trigger,data })=> {
           name="nama"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Nama Kecamatan</FormLabel>
+              <FormLabel>Nama Kelurahan</FormLabel>
               <FormControl>
-                <Input placeholder="Kecamatan" {...field} autoFocus={false}/>
+                <Input placeholder="Kelurahan" {...field} autoFocus={false} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -219,17 +301,21 @@ const FormData:FC<FromDataI> = ({ trigger,data })=> {
         />
 
         <div className="space-x-2">
-          <Button type="button" variant={"outline"} onClick={()=>destroyDrawerModal(ctx)}>Batal</Button>
-          <Button type="submit"> 
-           {isLoading && (
-            <ReloadIcon className="mr-2 h-3 w-3 animate-spin"/>
-           )}
-            {!data ? 'Tambah' : 'Ubah'}
+          <Button
+            type="button"
+            variant={"outline"}
+            onClick={() => destroyDrawerModal(ctx)}
+          >
+            Batal
+          </Button>
+          <Button type="submit">
+            {isLoading && <ReloadIcon className="mr-2 h-3 w-3 animate-spin" />}
+            {!data ? "Tambah" : "Ubah"}
           </Button>
         </div>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default FormData
+export default FormData;
